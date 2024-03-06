f922f2efca65dd9c1dfd7e384c2f9296baa2367c
Natalia-dot <nataliadurantegarcia@gmail.com>
2024-03-06 18:06:16 +0100

    Onclick handling in Class components

--- N1/src/Carousel.jsx
+++ N1/src/Carousel.jsx
@@ -8,6 +8,14 @@ export class Carousel extends Component {
     images: ["http://pets-images.dev-apis.com/pet/none.jpg"],
   };
 
+  changeIndex = (e) => {
+    this.setState({
+      active: +e.target.dataset.index,
+      //we use the + sign because everything coming from the DOM is string;
+      //the plus turns it to a number. Kind of like json.parse
+    });
+  };
+
   render() {
     const { active } = this.state;
     const { images } = this.props;
@@ -17,7 +25,11 @@ export class Carousel extends Component {
         <img src={images[active]} alt="animal hero" />
         <div className="carousel-smaller">
           {images.map((image, index) => (
+            //para que no me diga que tiene que tener una tecla asignada
+            // eslint-disable-next-line
             <img
+              onClick={this.changeIndex}
+              data-index={index}
               key={image}
               src={image}
               alt="pet"
