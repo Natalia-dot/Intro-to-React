8008e0b622a9d6dcacb7cbdd3e700d7e3e8d4095
Natalia-dot <nataliadurantegarcia@gmail.com>
2024-03-21 20:16:46 +0100

    Typescript setup and Modal refactor

---
+++ Lowprior-rerendering/src/Modal.tsx
@@ -0,0 +1,33 @@
+import React, {
+  MutableRefObject,
+  ReactElement,
+  useEffect,
+  useRef,
+} from "react";
+import { createPortal } from "react-dom";
+
+const Modal = ({ children }: { children: ReactElement }) => {
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
+  if (!elRef.current) {
+    elRef.current = document.createElement("div");
+  }
+
+  useEffect(() => {
+    const modalRoot = document.getElementById("modal");
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
+    modalRoot.appendChild(elRef.current);
+    //We have to add some conditional since there is a possibility that elRef or modal do not return their intended types
+    //so we have to check if the elements exist beforehand to let them do their appropriate operations
+    return () => {
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
+    };
+  }, []);
+
+  return createPortal(<div>{children}</div>, elRef.current);
+};
+
+export default Modal;
