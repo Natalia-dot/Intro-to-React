d44d28f7191ca3d60160a09ee86be9c2c494b778
Natalia-dot <nataliadurantegarcia@gmail.com>
2024-03-21 21:09:09 +0100

    Typescript files and configuration

---
+++ Lowprior-rerendering/try.tsx
@@ -0,0 +1,2 @@
+//this file is because the tsconfig was giving me an error so it was advised to add a tsx or ts file
+//in the same folder as the tsconfig file. This should be prevented by choosing the right folder
