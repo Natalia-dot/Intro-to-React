c7352891b66177c70952ed8136fcda0b04b9d8d1
Natalia-dot <nataliadurantegarcia@gmail.com>
2024-03-22 16:52:38 +0100

    Typing adopted pet context and making apiresponsestypes

---
+++ Lowprior-rerendering/src/AdoptedPetContext.ts
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+import { Pet } from "./APIResponsesTypes";
+
+const AdoptedPetContext = createContext<[Pet, (adoptedPet: Pet) => void] >([
+    {
+        id: 1111,
+        name: "Lola",
+        animal:"dog",
+        breed: "Poodle",
+        city: "Seattle",
+        description: "",
+        images: [],
+        state: "WA"
+    },
+    //eslint-disable-next-line
+    () => {},
+]);
+
+//could also be done like this but the initial value would be null and we'd have to type check everywhere else
+//const AdoptedPetContext = createContext<[Pet, (adoptedPet: Pet) => void] | null>(null);
+
+export default AdoptedPetContext;
