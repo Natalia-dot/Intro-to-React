052db4e1374d42732bb2aa49ed6ac257804a206e
Natalia-dot <nataliadurantegarcia@gmail.com>
2024-03-22 17:01:45 +0100

    Updated import for eslint

--- Lowprior-rerendering/src/Details.tsx
+++ Lowprior-rerendering/src/Details.tsx
@@ -1,12 +1,12 @@
 import { useNavigate, useParams } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { useContext, useState } from "react";
-import AdoptedPetContext from "./AdoptedPetContext";
 import Modal from "./Modal";
 import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
 import { PetAPIResponse } from "./APIResponsesTypes";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const Details = () => {
   const { id } = useParams();
